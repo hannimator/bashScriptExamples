a569591e4672095732b352d33d8c92c1b29ade71
Hannah <hannimator@gmail.com>
2021-01-31 19:59:01 +0000

    Update commonScript.sh

--- common/commonScript.sh
+++ common/commonScript.sh
@@ -4,4 +4,4 @@ text="releaseVersion86420"
 file="exampleFiles/textChanges.txt"
 
 delete_matching_lines "$text" "$file"
-run_installer "exampleFiles/installers/npp.7.8.8.Installer"
+run_installer "exampleFiles/installers/[yourInstallerHere]"
